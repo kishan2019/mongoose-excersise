3d8b1113f245b925a99c583972e68aeb2b91209e
kishan singh <48415357+kishan2019@users.noreply.github.com>
2020-01-03 12:41:39 +0530

    Add files via upload
    Added one more Route customer detail

--- routes/genres.js
+++ routes/genres.js
@@ -7,21 +7,23 @@ const Genre = mongoose.model('Genre', new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 3,
+    minlength: 5,
     maxlength: 50
   }
 }));
 
 router.get('/', async (req, res) => {
-  const genre = await Genre.find().sort('name');
-  res.send(genre);
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
+
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
+  
   res.send(genre);
 });
 
@@ -39,14 +41,18 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-   const genre = await Genre.findOneAndDelete(req.params.id);
-   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-   res.send(genre);
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
 });
 
 router.get('/:id', async (req, res) => {
   const genre = await Genre.findById(req.params.id);
+
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
   res.send(genre);
 });
 
